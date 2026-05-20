70cef6dbfb1e8aaadfac2063860f25908b588f13
fauzanekafbry-cpu <fauzanekafbry@gmail.com>
2026-05-21 02:08:57 +0700

    Register KonfigurasiVlanInterface job
    Register GUI Job for KonfigurasiVlanInterface.

--- jobs/__init__.py
+++ jobs/__init__.py
@@ -1,1 +1,5 @@
-from . import cisco_config_job
+from nautobot.apps.jobs import register_jobs
+from .cisco_config_job import KonfigurasiVlanInterface
+
+# Mendaftarkan GUI Job dari file sebelah secara resmi
+register_jobs(KonfigurasiVlanInterface)
